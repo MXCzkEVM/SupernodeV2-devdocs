236af3f990184cfc89f8ec33f68a8500810917c6
Sheen <9674168+sheenhx@users.noreply.github.com>
2024-03-25 12:50:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "MXCzkEVM";
-const projectName = "SupernodeV2-devdocs";
+const projectName = "Moonchain-devdocs";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
